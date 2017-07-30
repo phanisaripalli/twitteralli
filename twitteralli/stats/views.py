2c99fcf941cb7590d8e95841237c8a225256bc77
Phani <saripalli.phani@gmail.com>
2017-07-31 00:52:46 +0200

    added error handlers

--- twitteralli/stats/views.py
+++ twitteralli/stats/views.py
@@ -69,3 +69,20 @@ def logout(request):
 def login(request):
     context = {'activePage':'login'}
     return render(request, 'stats/login.html', context)        
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response    
+
+def foo(request):
+    context = {'activePage':'index'}    
+    return render(request, '404.html', context)     
